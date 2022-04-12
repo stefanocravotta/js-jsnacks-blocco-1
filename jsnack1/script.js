c223cffa3055a326d0e957203c265104c493afb6
stefanocravotta <you@example.com>
2022-04-12 17:38:39 +0200

    correzzion jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -7,7 +7,9 @@ const primoNum = parseInt(prompt("Inserisci un numero"));
 const secondoNum = parseInt(prompt("Inserisci un altro numero"));
 
 if(primoNum > secondoNum){
-    document.getElementById("numero-maggiore").innerHTML = primoNum;
-}else{
-    document.getElementById("numero-maggiore").innerHTML = secondoNum;
+    document.getElementById("numero-maggiore").innerHTML = `Il numero più alto inserito è ${primoNum}`;
+}else if(primoNum < secondoNum){
+    document.getElementById("numero-maggiore").innerHTML = `Il numero più alto inserito è ${secondoNum}`;
+}else if (primoNum === secondoNum){
+    document.getElementById("numero-maggiore").innerHTML = "Inserisci due numeri diversi tra loro";
 }
