fa6553c58b5301328a03b03a7aee5caada429386
stefanocravotta <you@example.com>
2022-04-12 16:23:38 +0200

    jsnack3 completo

---
+++ jsnack3/script.js
@@ -0,0 +1,14 @@
+/*
+ JSnack 3
+ Il software deve chiedere per 5 volte all’utente di inserire un numero.
+ Il programma stampa la somma di tutti i numeri inseriti.
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)
+*/
+
+const primaRichiesta = parseInt(prompt("Inserisci un numero"));
+const secondaRichiesta = parseInt(prompt("Inserisci un numero"));
+const terzaRichiesta = parseInt(prompt("Inserisci un numero"));
+const quartaRichiesta = parseInt(prompt("Inserisci un numero"));
+const quintaRichiesta = parseInt(prompt("Inserisci un numero"));
+
+document.getElementById("somma").append (primaRichiesta + secondaRichiesta + terzaRichiesta + quartaRichiesta + quintaRichiesta2)
