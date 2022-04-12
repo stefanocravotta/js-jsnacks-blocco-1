de6b753475d324fffd38406782833a6b6b41ea0b
stefanocravotta <you@example.com>
2022-04-12 17:30:12 +0200

    jsnack6 completo

---
+++ jsnack6/script.js
@@ -0,0 +1,11 @@
+/*
+ JSnack 6
+ Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
+*/
+
+const numeroUtente = parseInt(prompt("Inserisci un numero"));
+
+for(i = 0; i <= numeroUtente; i++){
+    
+    document.getElementById("cubo").append(` ${i ** 2} `)
+}
